fa42b49d3e72ef2b7dc854d52887c3e306e071df
Aarav Rathod <W0838110@myscc.ca>
2024-10-09 18:38:47 -0300

    Assignment 6 -Added angular forms -Checked my CRUD methods

---
+++ src/app/modify-game/modify-game.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modify-game',
+  standalone: true,
+  imports: [],
+  templateUrl: './modify-game.component.html',
+  styleUrl: './modify-game.component.css'
+})
+export class ModifyGameComponent {
+
+}
--- src/app/shared/mockGame.data.ts
+++ src/app/shared/mockGame.data.ts
@@ -3,14 +3,14 @@ import {Games} from "./models/games";
 export const game_list:Games[]=[
 
 
-  {id:0,title:"GOW",developer:"SANTA MONICO STUDIOS",release_date:"2018 Nov 27",character:"Kratos"},
+  {id:0,title:"GOW",developer:"SANTA MONICO STUDIOS",release_date:"2018 Nov 27",character:"Kratos",image:"gow.jpg"},
 
-  {id:1,title:"POP",developer:"UBSISOFT",release_date:"2018 Nov 27",character:"PRINCE"},
+  {id:1,title:"POP",developer:"UBSISOFT",release_date:"2018 Nov 27",character:"PRINCE",image:"pop.jpeg"},
 
 
-{id:2,title:"DANTE'S INFERNO",developer:"VISCERAL GAMES",release_date:"2018 Nov 27",character:"DANTE"},
+{id:2,title:"DANTE'S INFERNO",developer:"VISCERAL GAMES",release_date:"2018 Nov 27",character:"DANTE",image:"dante.jpeg"},
 
-{id:3,title:"BATMAN:ARKHMAN KNIGHT",developer:"ROCKSTEADY STUDIOS",release_date:"2015 Nov 27",character:"BATMAN"}
+{id:3,title:"BATMAN:ARKHMAN KNIGHT",developer:"ROCKSTEADY STUDIOS",release_date:"2015 Nov 27",character:"BATMAN",image:"batman.webp"}
 
 
 ]
