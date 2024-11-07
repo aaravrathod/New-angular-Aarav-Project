3117d6037e26337d2f814961681a1dc5b596c447
aaravrathod <W0838110@myscc.ca>
2024-11-07 15:34:27 -0400

    Assignment 7 -First 6 steps of assignment 7 completed

---
+++ src/app/services/in-memory-data.service.ts
@@ -0,0 +1,25 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Games} from "../shared/models/games";
+
+
+export class InMemoryDataService implements InMemoryDbService{
+
+    createDb():{Games: Games[]} {
+
+      const Games:Games[]=[
+
+        {id:0,title:"GOW",developer:"SANTA MONICO STUDIOS",release_date:"2018 Nov 27",character:"Kratos",image:"gow.jpg"},
+
+        {id:1,title:"POP",developer:"UBSISOFT",release_date:"2018 Nov 27",character:"PRINCE",image:"pop.jpeg"},
+
+
+        {id:2,title:"DANTE'S INFERNO",developer:"VISCERAL GAMES",release_date:"2018 Nov 27",character:"DANTE",image:"dante.jpeg"},
+
+        {id:3,title:"BATMAN:ARKHMAN KNIGHT",developer:"ROCKSTEADY STUDIOS",release_date:"2015 Nov 27",character:"BATMAN",image:"batman.webp"}
+      ];
+
+
+      return {Games}
+
+    }
+}
